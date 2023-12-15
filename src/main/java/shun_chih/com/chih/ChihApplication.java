da51fced268613174ab1d823d959f27dc45a5a42
m124578n <m23568n@gmail.com>
2023-12-15 10:02:07 +0800

    init spring boot project and add dependencies

---
+++ src/main/java/shun_chih/com/chih/ChihApplication.java
@@ -0,0 +1,13 @@
+package shun_chih.com.chih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shun_chih/com/chih/ChihApplicationTests.java
@@ -0,0 +1,13 @@
+package shun_chih.com.chih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
