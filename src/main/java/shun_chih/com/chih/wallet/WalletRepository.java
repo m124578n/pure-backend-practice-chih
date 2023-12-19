0bc4c456cf280e1a1a501d46c47e4d4e4393d968
m124578n <m23568n@gmail.com>
2023-12-19 11:38:19 +0800

    add wallet Repository

--- src/main/java/shun_chih/com/chih/wallet/WalletRepository.java
+++ src/main/java/shun_chih/com/chih/wallet/WalletRepository.java
@@ -1,4 +1,19 @@
 package shun_chih.com.chih.wallet;
 
-public class WalletRepository {
+import jakarta.persistence.LockModeType;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.stereotype.Repository;
+import shun_chih.com.chih.wallet.data.po.WalletPO;
+
+import java.util.Optional;
+
+@Repository
+public interface WalletRepository extends JpaRepository<WalletPO, Long>, JpaSpecificationExecutor<WalletPO> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Transactional
+    Optional<WalletPO> findByMemberId(Long memberId);
 }
